4757a0ef0ec5e8698b8bfdd51a243b710ac3c141
Calveron <48070599+Calveron@users.noreply.github.com>
2019-03-04 16:52:27 -0700

    Create outputSPI_tb.v

---
+++ outputSPI_tb.v
@@ -0,0 +1,9 @@
+// Testbench for ouputSPI.v
+// outputSPI.v is the SPI transmitting module
+//	inputs: in[7:0], clk, en
+//	outputs: out, en_out, clk_out
+//
+// recomendations to test:
+//	an eight-bit input, followed by an another, with appropriate timing
+//	
+
