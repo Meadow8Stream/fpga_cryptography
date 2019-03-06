e66590b8e8b4ae89284d86609864d1f47b220896
Calveron <48070599+Calveron@users.noreply.github.com>
2019-03-06 15:40:58 -0700

    Update outputSPI_tb.v

--- outputSPI_tb.v
+++ outputSPI_tb.v
@@ -1,9 +1,47 @@
-// Testbench for ouputSPI.v
-// outputSPI.v is the SPI transmitting module
-//	inputs: in[7:0], clk, en
-//	outputs: out, en_out, clk_out
+//-----------------------------------------------------------------------------
 //
-// recomendations to test:
-//	an eight-bit input, followed by an another, with appropriate timing
-//	
+// Title       : outputSPI
+// Author      : Caleb Ellington
+//
+//-----------------------------------------------------------------------------
+// 		
+// Purpose: To take Hash Table output and turn it into an SPI signal
+// Specifics: 
+//	Data line output
+// 	Enable line to say when talking
+//	Send clock to inplement symmetric clocks
+//
+//-----------------------------------------------------------------------------
+`timescale 1 ns / 1 ps
 
+//{module {outputSPI}}
+module outputSPI ();
+input in[7:0];
+input en;
+input clk;
+output out;
+output en_out;
+output clk_out;
+
+	reg sr[7:0];					// index 0 = msb
+	
+always(posedge clk)
+	begin
+		if (en) 
+			begin
+				sr <= in;
+				
+				// create and send sync'ed clock
+				$10 clk_out <= clk;
+				
+				// send enable
+				en_out <= en;	
+				// construct signal
+				out <= sr;
+			end
+			else
+			begin
+				sr <= 0;
+			end
+	end
+endmodule
