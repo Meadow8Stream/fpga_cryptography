9c238feab8b09b6d966aa2106f70c224e49ed974
Calveron <48070599+Calveron@users.noreply.github.com>
2019-03-06 16:21:43 -0700

    Update outputSPI.v

--- outputSPI.v
+++ outputSPI.v
@@ -1,7 +1,6 @@
 //-----------------------------------------------------------------------------
 //
 // Title       : outputSPI
-// Design      : outputSPI
 // Author      : Caleb Ellington
 //
 //-----------------------------------------------------------------------------
@@ -16,34 +15,42 @@
 `timescale 1 ns / 1 ps
 
 //{module {outputSPI}}
-module outputSPI ();
+module outputSPI (in, en, clk, out, en_out, clk_out);
 input in[7:0];
 input en;
 input clk;
-output out;
-output en_out;
-output clk_out;
+output reg out;
+output reg en_out;
+output reg clk_out;
 
-	reg sr[7:0];					// index 0 = msb
+reg sr[7:0];					// index 7 = msb
 	
-always(posedge clk)
+always@(posedge clk)
 	begin
 		if (en) 
 		begin
 			sr <= in;
 
 			// create and send sync'ed clock
-			$10 clk_out <= clk;
+			#10 clk_out <= clk;
 
 			// send enable
-			en_out <= 1'b1;	
+			en_out <= en;	
 			
 			// construct signal
-			out <= sr;
+			#1 out <= sr[0];
+			#1 out <= sr[1];
+			#1 out <= sr[2];
+			#1 out <= sr[3];
+			#1 out <= sr[4];
+			#1 out <= sr[5];
+			#1 out <= sr[6];
+			#1 out <= sr[7];
+			
 		end
 		else
 		begin
-			sr <= 0;
+			sr <= 8'b00000000;
 		end
 	end
 endmodule
