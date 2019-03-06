bb04e97fb7dcde75fe8f39f9f341e647e231ab2e
Justus <justus.engstrom@gmail.com>
2019-03-06 12:13:31 -0700

    Create parsing.v

---
+++ parsing.v
@@ -0,0 +1,3 @@
+/* Please create a parsing program that can take in a 16 byte stream of 1s and 0s, 
+store the stream as one 16 byte register of data, and compare this data to presored data.
+For now, please use constants for the prestored data that you may randomly chose just for testing purposes.*/
