04cc7f0a5775c5f0d44e0aa83033a167d4a71014
Calveron <48070599+Calveron@users.noreply.github.com>
2019-03-07 17:14:51 -0700

    Update outputSPI_tb.v

--- outputSPI_tb.v
+++ outputSPI_tb.v
@@ -12,32 +12,37 @@
 `timescale 1 ns / 1 ps
 
 module outputSPI_tb;
-reg clk, en;
+reg clk, rst;
 reg [7:0]in;
 
-wire [3:0]out
+reg out;
+reg sent;
 wire clk_out, en_out;
 
 outputSPI UUT (
 .clk (clk)
-.en (en)
+.rst (rst)
 .in (in)
 .out (out)
 .clk_out (clk_out)
 .en_out (en_out)
+.sent (sent)
 );				
 
 always
-	#10 clk = ~clk;
-	#5 en <= 1'b1;
-	#1 in <= 8'b00000000;
-	#1 in <= 8'b00000001;
-	#1 in <= 8'b00000010;
-	#1 in <= 8'b00000011;
+	begin
+	#1 clk = ~clk;
+	#5 rst <= 1'b1;
+	end
 	
-	initial begin
-		$display("\t\ttime,\tclk,\ten,\tin,\tout"); 
-		$monitor("%d,\t%b,\t%b,\t%b,\t%d",$time, clk,en,in,out);
+initial
+	begin
+	#1 in <= 8'b00000000;
+	end
+	end
+initial begin
+	$display("\t\ttime,\tclk,\ten,\tin,\tout"); 
+	$monitor("%d,\t%b,\t%b,\t%b,\t%d",$time, clk,en,in,out);
 	end
 
 endmodule
