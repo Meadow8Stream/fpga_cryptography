97729d88c4e19a7e7ebaa9e1d0bda228d24235e2
wmchaley <48070575+wmchaley@users.noreply.github.com>
2019-03-25 15:33:47 -0600

    Update parsing.v

--- parsing.v
+++ parsing.v
@@ -2,18 +2,25 @@
 store the stream as one 16 byte register of data, and compare this data to presored data.
 For now, please use constants for the prestored data that you may randomly chose just for testing purposes.*/
 
-module parse(input clk, input end_of_sequence, input in_data);
+module parse(input clk, input end_of_sequence, input info);
   reg[127:0] pdata;
   output [127:0] result;
-
+  
+  
 //store 16 byte stream
 
 always @(posedge clk) begin
+  case(info)
+      2'b00 : pdata <= pdata; 
+      2'b01 : pdata <={pdata[126:0, 1};
+      2'b10 : pdata <={pdata[126:0, 0};
+    default:
+  endcase 
   pdata <= {pdata[126:0, in_data};
 end
 
 always @(posedge clk) begin
-  if(end_of_sequence ) begin
+  if(end_of_sequence) begin
     result <= pdata;
   end
 end
