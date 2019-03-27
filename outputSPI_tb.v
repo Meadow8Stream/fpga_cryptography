8543ae25c24af57703d507ea76e2e312dd0152d3
Calveron <48070599+Calveron@users.noreply.github.com>
2019-03-27 15:59:23 -0600

    Update outputSPI_tb.v

--- outputSPI_tb.v
+++ outputSPI_tb.v
@@ -12,41 +12,51 @@
 `timescale 1 ns / 1 ps
 
 module outputSPI_tb;
-reg clk, rst;
+reg clk, rst, en;
 reg [7:0]in;
 
 reg out;
 reg sent;
-wire clk_out, en_out;
+wire clk_out, en_out;  
+integer i;
 
 outputSPI UUT (
-.clk (clk)
-.rst (rst)
-.in (in)
-.out (out)
-.clk_out (clk_out)
-.en_out (en_out)
-.sent (sent)
-);				
-initial begin
-	clk <= 1'b0;
-	rst <= 1'b0;
-	in <= 8'b11111111;
-	
+.clk 		(clk),
+.rst 		(rst),	
+.en  		(en),
+.in  		(in),
+.out 	 	(out),
+.clk_out 	(clk_out),
+.en_out 	(en_out),
+.sent 		(sent)
+);		
+
+initial 
+	begin
+	clk = 1'b0;
+	rst = 1'b0;
+	in = 8'b11111111; 
+	i <= 0;
+end
+
 always
 	begin
-	#1 clk = ~clk;
-	end
+	#1 clk <= ~clk;
+end
 	
-always begin
-	for (i = 0; i < 256; i = i + 1) begin
+always
+	begin 
+	if (i < 5) begin
 		in <= in + 8'b00000001;
+		i <= i + 1;
+	end else begin
+		i <= 0;
 	end
 end
 	
 initial begin
-	$display("\t\ttime,\tclk,\ten,\tin,\tout"); 
-	$monitor("%d,\t%b,\t%b,\t%b,\t%d",$time, clk,en,in,out);
-	end
+	$display("\t\ttime,\tclk,\trst,\tin,\tout"); 
+	$monitor("%d,\t%b,\t%b,\t%b,\t%d",$time, clk,rst,in,out);
+end
 
 endmodule
