9382777cb9adad0a87d157e959aafb770467b65f
Calveron <48070599+Calveron@users.noreply.github.com>
2019-03-28 17:53:26 -0600

    Update outputSPI.v

--- outputSPI.v
+++ outputSPI.v
@@ -27,28 +27,29 @@ module outputSPI (in, rst, en, clk, out, en_out, clk_out, sent);
 	
 	// internal variables
 	reg [7:0] sr;					// index 7 = msb	  
-	reg [1:0] counter1 = 2'b00;
-	reg [1:0] counter2 = 2'b00;
+	reg [3:0] counter1;
+	reg [1:0] counter2;
 	
-	// initialize variables
-	
+	// initialize variables	 
+	initial begin 
+		clk_out = clk;
+	end	
+		
 	always@(posedge clk, rst)
 	begin
 		if (rst) begin
-			counter1 <= 2'b00;
+			counter1 <= 4'b0000;
 			counter2 <= 2'b00;
 			out 	 <= 1'b0;   
-			en_out   <= 1'b0;
+			en_out   <= 1'b1;
 			sent 	 <= 1'b1;  
-			clk_out  <= 1'b0;
 			sr 	 <= 8'b00000000;
 		end else if (en) begin
-			counter1 <= 2'b00;
+			counter1 <= 4'b0000;
 			counter2 <= 2'b00;
 			out 	 <= 1'b0;   
-			en_out   <= 1'b0;
-			sent 	 <= 1'b1;  
-			clk_out  <= 1'b0;
+			en_out   <= 1'b1;
+			sent 	 <= 1'b1; 
 			sr 	 <= 8'b00000000;
 		end else begin  
 			// when sent is 0, then outputSPI is transmitting and not accepting
@@ -63,31 +64,30 @@ module outputSPI (in, rst, en, clk, out, en_out, clk_out, sent);
 				sr <= in;
 			end
 			// send enable
-			en_out <= 1'b1;	
+			en_out <= 1'b0;	
 			
 			// construct signal
 			// Basically calling the correct send task to put the proper bit on the SPI line
 			if(counter1 < 8) begin
-				if(counter2 == 2'b00) begin
-					clk_out <= clk;
-					en_out 	<= 1'b0;
-					out 	<= 1'b1;
+				if(counter2 == 2'b00) begin	 
+					counter1 <= counter1 + 4'b0001;
+					en_out 	 <= 1'b0;
+					out 	 <= 1'b1;
 					counter2 <= 2'b01;
-				end else if (counter2 == 2'b01) begin
-					clk_out <= clk;
-					en_out  <= 1'b0;
-					out	<= sr[0];
-					sr 	<= sr >> 1;
+				end else if (counter2 == 2'b01) begin  
+					en_out   <= 1'b0;
+					out		 <= sr[0];
+					sr 		 <= sr >> 1;
 					counter2 <= 2'b10;
 				end else if (counter2 == 2'b10) begin
-					clk_out <= clk;
-					en_out  <= 1'b0;
-					out 	<= 1'b0;
+					en_out   <= 1'b0;
+					out 	 <= 1'b0;
 					counter2 <= 2'b00;
 				end
-				counter1 <= counter1 + 2'b01;
-			end else begin
-				counter1 <= 2'b00;
+			end else begin	  
+				out 		 <= 1'b0;
+				counter1 	 <= 4'b0000;
+				sent 		 <= 1'b1;
 			end
 		end
 	end
