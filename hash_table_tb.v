bcc4ef61635eb449e9aa219a817d81ed558faf6f
BobbiJean <48070592+BobbiJean@users.noreply.github.com>
2019-03-28 17:15:06 -0600

    Update hash_table_tb.v

--- hash_table_tb.v
+++ hash_table_tb.v
@@ -1,1 +1,32 @@
 
+`timescale 1 ns / 1 ps
+module TestBench;
+				   	  		   
+	reg [7:0] val;
+	reg clock, reset;
+	wire [8:0] flag;	
+		
+	//module
+		HashTree HashTree(
+		.clock(clock),
+		.reset(reset),
+		.val(val),
+		.flag(flag)
+		);  
+		
+	initial begin
+	clock = 0;
+	flag = 0;
+	val = 8'b01000000;	
+	reset = 0;
+	end
+	
+	always begin
+		#1 clock = ~clock;
+	end
+	
+	//always
+	//	#4 reset = ~reset;
+	
+	
+endmodule
