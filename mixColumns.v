0788f537c8581677be603c9f71a7a93a9ed76d5d
Calveron <48070599+Calveron@users.noreply.github.com>
2019-04-01 17:02:31 -0600

    Update mixColumns.v

--- mixColumns.v
+++ mixColumns.v
@@ -7,7 +7,7 @@ input clk;
 input rst;
 
 input 	   [7:0] in[15:0];			// input represented as 16 bytes
-output reg [7:0] out[15:0];		// output represented as 16 bytes
+output reg [7:0] out[15:0];			// output represented as 16 bytes
 
 wire two_GF;
 wire three_GF;
@@ -16,13 +16,25 @@ assign three_GF = 8'b00000011;
 
 always@(posedge clk, rst)
 	// word "times" in comments means galois field multiplication using (2^8)
+	// symbol "+" means xor
 	begin
 		if (~rst) begin
 			// mixing of columns
+			// row 1 calculations:
+			// two times the current byte + three times the byte to the right +
+			// two bytes to the right + three bytes to the right
+			out[0] <= multGF(two_GF, in[0]) ^ multGF(three_GF, in[1]) ^ in[2] ^ in[3];
+			out[1] <= multGF(two_GF, in[1]) ^ multGF(three_GF, in[1]) ^ in[2] ^ in[3];
+			out[2] <= multGF(two_GF, in[2]) ^ multGF(three_GF, in[1]) ^ in[2] ^ in[3];
+			out[3] <= multGF(two_GF, in[3]) ^ multGF(three_GF, in[1]) ^ in[2] ^ in[3];
+			
+			// row 2 calculations:
+			//
+			
 		end
 	end
 
-task multGFBasic;
+task multGF;
 	// I/O + variables
 	input [7:0] a;
 	input [7:0] b;
