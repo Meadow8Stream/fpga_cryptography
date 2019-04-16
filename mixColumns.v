e55c1e3ce97411c2a4b1edb950f2360e1a76e3c1
Calveron <48070599+Calveron@users.noreply.github.com>
2019-04-16 14:50:40 -0600

    Update mixColumns.v

--- mixColumns.v
+++ mixColumns.v
@@ -6,71 +6,41 @@ module mixColumns(clk, rst, in, out);
 input clk;
 input rst;
 
-input 	   [7:0] in[15:0];			// input represented as 16 bytes
-output reg [7:0] out[15:0];			// output represented as 16 bytes
+input 	   [7:0] data_in[15:0];			// input represented as 16 bytes
+output reg [7:0] data_out[15:0];			// output represented as 16 bytes
 
-wire two_GF;
-wire three_GF;
-assign two_GF	= 8'b00000010;
-assign three_GF = 8'b00000011;
-
-always@(posedge clk, rst)
-	// word "times" in comments means galois field multiplication using (2^8)
-	// symbol "+" means xor
-	begin
-		if (~rst) begin
-			// mixing of columns
-			// row 1 calculations:
-			// two times the current byte + three times the byte to the right +
-			// two bytes to the right + three bytes to the right
-			out[0] <= multGF(two_GF, in[0], rst, 1) ^ multGF(three_GF, in[1], rst, 1) ^ in[2] ^ in[3];
-			out[1] <= multGF(two_GF, in[1], rst, 1) ^ multGF(three_GF, in[1], rst, 1) ^ in[2] ^ in[3];
-			out[2] <= multGF(two_GF, in[2], rst, 1) ^ multGF(three_GF, in[1], rst, 1) ^ in[2] ^ in[3];
-			out[3] <= multGF(two_GF, in[3], rst, 1) ^ multGF(three_GF, in[1], rst, 1) ^ in[2] ^ in[3];
-			
-			// row 2 calculations:
-			//
-			
-		end
-	end
-
-task multGF;
-	// I/O + variables
-	input [7:0] a;
-	input [7:0] b;
-	input rst;
-	input gf_en;
-	output reg [7:0] p;
+wire[7:0] sr_gf1[15:0];				// will hold original input or galois x2 multiplication
+wire[7:0] sr_gf1[15:0];				// will hold galois x2 multiplication
+wire[7:0] sr_gf1[15:0];				// will hold galois x3 multiplication
 	
-	reg carry;
-	reg [3:0] count = 4'b0000;
-	reg [4:0] p_irr = 8'b11011;
+reg [7:0] loop;
 	
-	always@(posedge clk) begin
-		if (rst) begin
-			count <= 4'0000;
-			p <= 8'00000000;
-			return;
-		end else if (gf_en) begin
-			count <= 4'b0000;
-			p <= 8'b00000000;
-			gf_en <= 0;
-		end else if (count < 4'b1000) begin
-			if(b[0]) begin
-				p <= p ^b;
-			end
-			b <= b >> 1;
-			carry <= a[0];
-			a <= a << 1;
-			
-			if(carry) begin
-				a <= a ^ p_irr;
-			end
-			count <= count + 1;
-		end else begin
-			count <= 0;
-		end
+generate
+	for (loop = 0; loop < 15; loop = loop + 1) begin
+		assign sr_gf1[loop] = data_in[loop];
+		assign sr_gf2[loop] = data_in[0][7] ? ((data_in[loop] << 1) ^ 8'h1b) : (data_in[loop] << 1);
+		assign sr_gf3[loop] = sr_gf2[loop] ^ data_in[loop];
 	end
-endtask
+endgenerate	
+	
+	assign data_out[0] = sr_gf2[0] ^ sr_gf3[1] ^ sr_gf1[2] ^ sr_gf1[3];
+	assign data_out[1] = sr_gf1[0] ^ sr_gf2[1] ^ sr_gf3[2] ^ sr_gf1[3];
+	assign data_out[2] = sr_gf1[0] ^ sr_gf1[1] ^ sr_gf2[2] ^ sr_gf3[3];
+	assign data_out[3] = sr_gf3[0] ^ sr_gf1[1] ^ sr_gf1[2] ^ sr_gf2[3];
+	
+	assign data_out[4] = sr_gf2[4] ^ sr_gf3[5] ^ sr_gf1[6] ^ sr_gf1[7];
+	assign data_out[5] = sr_gf1[4] ^ sr_gf2[5] ^ sr_gf3[6] ^ sr_gf1[7];
+	assign data_out[6] = sr_gf1[4] ^ sr_gf1[5] ^ sr_gf2[6] ^ sr_gf3[7];
+	assign data_out[7] = sr_gf3[4] ^ sr_gf1[5] ^ sr_gf1[6] ^ sr_gf2[7];
+	
+	assign data_out[8] = sr_gf2[8] ^ sr_gf3[9] ^ sr_gf1[10] ^ sr_gf1[11];
+	assign data_out[9] = sr_gf1[8] ^ sr_gf2[9] ^ sr_gf3[10] ^ sr_gf1[11];
+	assign data_out[10] = sr_gf1[8] ^ sr_gf1[9] ^ sr_gf2[10] ^ sr_gf3[11];
+	assign data_out[11] = sr_gf3[8] ^ sr_gf1[9] ^ sr_gf1[10] ^ sr_gf2[11];
+	
+	assign data_out[12] = sr_gf2[12] ^ sr_gf3[13] ^ sr_gf1[14] ^ sr_gf1[15];
+	assign data_out[13] = sr_gf1[12] ^ sr_gf2[13] ^ sr_gf3[14] ^ sr_gf1[15];
+	assign data_out[14] = sr_gf1[12] ^ sr_gf1[13] ^ sr_gf2[14] ^ sr_gf3[15];
+	assign data_out[15] = sr_gf3[12] ^ sr_gf1[13] ^ sr_gf1[14] ^ sr_gf2[15];
 	
 endmodule
